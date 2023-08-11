6f71e5349c994a65dcdbfd24e8195a34ad945e02
jzapata <jzapata@lifefile.net>
2023-08-10 23:20:12 -0500

    requests (text, html, JSON)

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello world!')
 });
+
+app.get('/messages', (req, res) => {
+    res.send('<ul><li>Item One</li><li>Item Two</li></ul>');
+});
+
+app.get('/object', (req, res) => {
+    res.send({name: 'Julian'});
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
