5803316f231c2126d5049b417db9878096036a84
jzapata <jzapata@lifefile.net>
2023-08-11 04:59:34 -0500

    add express.json middleware + POST new friend request

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const friends = [
     {id: 3, name: 'Friend3'},
 ]
 
+app.use((req, res, next) => {
+    // Here when the request comes in
+    const start = Date.now();
+    next(); // Call to next middleware
+    // Here when the response is back
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+// express.json is a middleware It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 });
@@ -28,10 +40,26 @@ app.get('/friends/:friendId', (req, res) => {
         res.status(200).json(friend);
     } else {
         res.status(404).json({
-            error: "Friend does not exist"
-        })
+            error: 'Friend does not exist'
+        });
     }
 
+});
+
+app.post('/friends', (req, res) => {
+
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friend name'
+        });
+    }
+
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    }
+    friends.push(newFriend);
+    res.json(newFriend);
 });
 
 app.get('/object', (req, res) => {
