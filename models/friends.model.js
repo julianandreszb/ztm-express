82a28b30a0ef3d0e29807942ae76469965b1de78
jzapata <jzapata@lifefile.net>
2023-08-14 23:08:03 -0500

    add MVC

---
+++ models/friends.model.js
@@ -0,0 +1,7 @@
+const friends = [
+    {id: 1, name: 'Friend1'},
+    {id: 2, name: 'Friend2'},
+    {id: 3, name: 'Friend3'},
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
+const messagesController = require('./controllers/messages.controller');
+const friendsController = require('./controllers/friends.controller');
 
 const app = express();
 const PORT = 3000;
-
-const friends = [
-    {id: 1, name: 'Friend1'},
-    {id: 2, name: 'Friend2'},
-    {id: 3, name: 'Friend3'},
-]
 
 app.use((req, res, next) => {
     // Here when the request comes in
@@ -25,46 +21,12 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
-app.get('/messages', (req, res) => {
-    res.send('<ul><li>Item One</li><li>Item Two</li></ul>');
-});
+app.get('/messages', messagesController.getMessages);
+app.post('/messages', messagesController.postMessage);
 
-app.get('/friends', (req, res) => {
-    res.json(friends);
-});
-
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend);
-    } else {
-        res.status(404).json({
-            error: 'Friend does not exist'
-        });
-    }
-
-});
-
-app.post('/friends', (req, res) => {
-
-    if (!req.body.name) {
-        return res.status(400).json({
-            error: 'Missing friend name'
-        });
-    }
-
-    const newFriend = {
-        name: req.body.name,
-        id: friends.length
-    }
-    friends.push(newFriend);
-    res.json(newFriend);
-});
-
-app.get('/object', (req, res) => {
-    res.send({name: 'Julian'});
-});
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend);
+app.post('/friends', friendsController.postFriend);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
