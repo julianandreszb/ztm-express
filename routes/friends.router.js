b62f2e70dd62e76cf23d0c10299f14692e45bbf5
jzapata <jzapata@lifefile.net>
2023-08-14 23:35:23 -0500

    add friends and messages routers

---
+++ routes/friends.router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const friendsRouter = express.Router();
+const friendsController = require('./../controllers/friends.controller');
+
+friendsRouter.use((req, res, next) => {
+    console.log(`IP address: ${req.ip}`);
+    next();
+});
+friendsRouter.get('/', friendsController.getFriends);
+friendsRouter.get('/:friendId', friendsController.getFriend);
+friendsRouter.post('/', friendsController.postFriend);
+
+module.exports = friendsRouter;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const messagesController = require('./controllers/messages.controller');
-const friendsController = require('./controllers/friends.controller');
+
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 const PORT = 3000;
@@ -11,22 +12,13 @@ app.use((req, res, next) => {
     next(); // Call to next middleware
     // Here when the response is back
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
-
 // express.json is a middleware It parses incoming requests with JSON payloads and is based on body-parser.
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-});
-
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
-
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId', friendsController.getFriend);
-app.post('/friends', friendsController.postFriend);
+app.use('/messages', messagesRouter);
+app.use('/friends', friendsRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
