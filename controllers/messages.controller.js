f4c983face66a71d59a63111fa9051aa3283afd3
jzapata <jzapata@lifefile.net>
2023-08-15 23:52:56 -0500

    sending files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Item One</li><li>Item Two</li></ul>');
+    //res.send('<ul><li>Item One</li><li>Item Two</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', 'img.png'));
 }
 
 function postMessage(req, res) {
